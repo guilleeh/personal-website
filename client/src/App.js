53615661963020987acddb4d1dcc8ebbf4fb1aac
guilleeh <guilleeh@uci.edu>
2019-08-23 14:55:33 -0700

    Added main component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Nav from './components/layout/Nav';
+import Main from './components/layout/Main';
 
 const App = () => {
     return (
         <Fragment>
             <Nav />
+            <Main />
         </Fragment>
     );
 };
---
+++ client/src/components/layout/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+const Main = () => {
+    return <div style={style} />;
+};
+
+const style = {
+    position: 'absolute',
+    top: '10',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    background: 'rgba(33,125,187,0.8)'
+};
+
+export default Main;
